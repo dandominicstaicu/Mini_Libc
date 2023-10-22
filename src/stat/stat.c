a58c2efa786c5cc22f5aa88b95acd695f1539720
dandominicstaicu <dando.ds11@gmail.com>
2023-10-22 15:45:33 +0300

    working on stat

--- src/stat/stat.c
+++ src/stat/stat.c
@@ -4,9 +4,25 @@
 #include <internal/syscall.h>
 #include <fcntl.h>
 #include <errno.h>
+#include <unistd.h>
 
 int stat(const char *restrict path, struct stat *restrict buf)
 {
-	/* TODO: Implement stat(). */
-	return -1;
+	if (!path || !buf) {
+		errno = EINVAL;
+		return -1;
+	}
+
+	if (syscall(__NR_access, path, F_OK) < 0) {
+		errno = ENOENT;
+		return -1;
+	}
+
+	int result = syscall(__NR_stat, path, buf);
+
+	if (result < 0) {
+		errno = -result;
+	}
+
+	return result;
 }
